42e8ad67a4b7d3fae324fd5701f9b6e71fffaa6c
Fagner Pereira <fagnerfpr@gmail.com>
2016-08-22 17:35:52 -0300

    Autorizando twilio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,53 +3,53 @@ class ApplicationController < ActionController::Base
 
   def voice_body
     xml = '<?xml version="1.0" encoding="UTF-8" ?>
-<Response>
-  <Say voice="woman" language="pt-BR">Olá, insira o código a seguir na tela de assinatura. O código de 6 letras é:</Say>
-<Pause /><Say voice="woman" language="pt-BR">B de Bola</Say>
-<Pause /><Say voice="woman" language="pt-BR">C de Casa</Say>
-<Pause /><Say voice="woman" language="pt-BR">D de Dado</Say>
-<Pause /><Say voice="woman" language="pt-BR">F de Faca</Say>
-<Pause /><Say voice="woman" language="pt-BR">G de Gato</Say>
-<Pause /><Say voice="woman" language="pt-BR">H de Hotel</Say>
-<Pause /><Say voice="woman" language="pt-BR">J de Jacaré</Say>
-<Pause /><Say voice="woman" language="pt-BR">K de Karaokê</Say>
-<Pause /><Say voice="woman" language="pt-BR">L de Laranja</Say>
-<Pause /><Say voice="woman" language="pt-BR">M de Maria</Say>
-<Pause /><Say voice="woman" language="pt-BR">Êênii de Navio</Say>
-<Pause /><Say voice="woman" language="pt-BR">P de Panela</Say>
-<Pause /><Say voice="woman" language="pt-BR">Q de Queijo</Say>
-<Pause /><Say voice="woman" language="pt-BR">Éérri de Rato</Say>
-<Pause /><Say voice="woman" language="pt-BR">S de Sapo</Say>
-<Pause /><Say voice="woman" language="pt-BR">Têê de Tatu</Say>
-<Pause /><Say voice="woman" language="pt-BR">Vêêê de Vassoura</Say>
-<Pause /><Say voice="woman" language="pt-BR">W de Wilson</Say>
-<Pause /><Say voice="woman" language="pt-BR">X de Xadrez</Say>
-<Pause /><Say voice="woman" language="pt-BR">Z de Zebra</Say>
-  <Pause />
-  <Say voice="woman" language="pt-BR">Repetindo</Say>
-<Pause /><Say voice="woman" language="pt-BR">B de Bola</Say>
-<Pause /><Say voice="woman" language="pt-BR">C de Casa</Say>
-<Pause /><Say voice="woman" language="pt-BR">D de Dado</Say>
-<Pause /><Say voice="woman" language="pt-BR">F de Faca</Say>
-<Pause /><Say voice="woman" language="pt-BR">G de Gato</Say>
-<Pause /><Say voice="woman" language="pt-BR">H de Hotel</Say>
-<Pause /><Say voice="woman" language="pt-BR">J de Jacaré</Say>
-<Pause /><Say voice="woman" language="pt-BR">K de Karaokê</Say>
-<Pause /><Say voice="woman" language="pt-BR">L de Laranja</Say>
-<Pause /><Say voice="woman" language="pt-BR">M de Maria</Say>
-<Pause /><Say voice="woman" language="pt-BR">Êênii de Navio</Say>
-<Pause /><Say voice="woman" language="pt-BR">P de Panela</Say>
-<Pause /><Say voice="woman" language="pt-BR">Q de Queijo</Say>
-<Pause /><Say voice="woman" language="pt-BR">Éérri de Rato</Say>
-<Pause /><Say voice="woman" language="pt-BR">S de Sapo</Say>
-<Pause /><Say voice="woman" language="pt-BR">Têê de Tatu</Say>
-<Pause /><Say voice="woman" language="pt-BR">Vêêê de Vassoura</Say>
-<Pause /><Say voice="woman" language="pt-BR">W de Wilson</Say>
-<Pause /><Say voice="woman" language="pt-BR">X de Xadrez</Say>
-<Pause /><Say voice="woman" language="pt-BR">Z de Zebra</Say>
-  <Pause />
-  <Say voice="woman" language="pt-BR">Obrigada.</Say>
-</Response>'
+            <Response>
+              <Say voice="woman" language="pt-BR">Olá, insira o código a seguir na tela de assinatura. O código de 6 letras é:</Say>
+            <Pause /><Say voice="woman" language="pt-BR">B de Bola</Say>
+            <Pause /><Say voice="woman" language="pt-BR">C de Casa</Say>
+            <Pause /><Say voice="woman" language="pt-BR">D de Dado</Say>
+            <Pause /><Say voice="woman" language="pt-BR">F de Faca</Say>
+            <Pause /><Say voice="woman" language="pt-BR">G de Gato</Say>
+            <Pause /><Say voice="woman" language="pt-BR">H de Hotel</Say>
+            <Pause /><Say voice="woman" language="pt-BR">J de Jacaré</Say>
+            <Pause /><Say voice="woman" language="pt-BR">K de Karaokê</Say>
+            <Pause /><Say voice="woman" language="pt-BR">L de Laranja</Say>
+            <Pause /><Say voice="woman" language="pt-BR">M de Maria</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Êênii de Navio</Say>
+            <Pause /><Say voice="woman" language="pt-BR">P de Panela</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Q de Queijo</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Éérri de Rato</Say>
+            <Pause /><Say voice="woman" language="pt-BR">S de Sapo</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Têê de Tatu</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Vêêê de Vassoura</Say>
+            <Pause /><Say voice="woman" language="pt-BR">W de Wilson</Say>
+            <Pause /><Say voice="woman" language="pt-BR">X de Xadrez</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Z de Zebra</Say>
+              <Pause />
+              <Say voice="woman" language="pt-BR">Repetindo</Say>
+            <Pause /><Say voice="woman" language="pt-BR">B de Bola</Say>
+            <Pause /><Say voice="woman" language="pt-BR">C de Casa</Say>
+            <Pause /><Say voice="woman" language="pt-BR">D de Dado</Say>
+            <Pause /><Say voice="woman" language="pt-BR">F de Faca</Say>
+            <Pause /><Say voice="woman" language="pt-BR">G de Gato</Say>
+            <Pause /><Say voice="woman" language="pt-BR">H de Hotel</Say>
+            <Pause /><Say voice="woman" language="pt-BR">J de Jacaré</Say>
+            <Pause /><Say voice="woman" language="pt-BR">K de Karaokê</Say>
+            <Pause /><Say voice="woman" language="pt-BR">L de Laranja</Say>
+            <Pause /><Say voice="woman" language="pt-BR">M de Maria</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Êênii de Navio</Say>
+            <Pause /><Say voice="woman" language="pt-BR">P de Panela</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Q de Queijo</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Éérri de Rato</Say>
+            <Pause /><Say voice="woman" language="pt-BR">S de Sapo</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Têê de Tatu</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Vêêê de Vassoura</Say>
+            <Pause /><Say voice="woman" language="pt-BR">W de Wilson</Say>
+            <Pause /><Say voice="woman" language="pt-BR">X de Xadrez</Say>
+            <Pause /><Say voice="woman" language="pt-BR">Z de Zebra</Say>
+              <Pause />
+              <Say voice="woman" language="pt-BR">Obrigada.</Say>
+            </Response>'
 
     render xml: xml
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'voice_body' => 'application#voice_body'
+
+  root to: 'application#home'
 end
